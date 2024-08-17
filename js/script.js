af41f10583181c229cad7b6a49b969cd1a996b0e
r_souza <Rafaelrosod@outlook.com>
2024-08-17 18:43:59 -0300

    Projeto em andamento.

---
+++ js/script.js
@@ -0,0 +1,40 @@
+document.getElementById("teste", ).addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/itadori.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
+document.getElementById("teste-1").addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/yuta.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
+document.getElementById("teste-2").addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/Sukuna.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
+document.getElementById("teste-3").addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/satoru.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
+document.getElementById("teste-4").addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/nanami.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
+document.getElementById("teste-5").addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/Geto.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
+document.getElementById("teste-6").addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/choso.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
+document.getElementById("teste-7").addEventListener("click", function(){
+    this.style.backgroundImage = "url('../assets/maki.jpeg')";
+    this.style.backgroundSize = "cover";  
+    this.style.backgroundColor = "transparent";
+});
